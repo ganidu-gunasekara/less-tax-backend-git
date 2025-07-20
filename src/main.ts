3b7919992233515ddadfa77919680eda14e30db7
ganidugunasekara91@gmail.com <ganidugunasekara91@gmail.com>
2025-07-20 19:21:06 +0530

    cors issue fix

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
     credentials: true,
   });
-  
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
